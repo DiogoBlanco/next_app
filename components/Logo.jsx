c6e9e9a58fc89fe4dbd491f569b5f18c78154ad1
DiogoBlanco <diogoblanco@outlook.com.br>
2023-10-26 11:01:28 -0300

    css

--- components/Logo.jsx
+++
@@ -1,3 +0,0 @@
-export default function Logo () {
-  return <img src="../public/logo.jpg" alt="logo" width={100} height={50}></img>
-}
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,26 @@
-import "./Navbar.css"
-import Link from "next/link"
-import Logo from "./Logo"
+import "./Navbar.css";
+import Link from "next/link";
 
 export default function Navbar() {
-  return <nav className="navbar">
-    <ul className="nav-ul">
-      <Logo/>
-      <Link className="nav-li" href={"/"}>Home</Link>
-      <Link className="nav-li" href={"/contact"}>Contato</Link>
-      <li className="nav-li">Sobre</li>
-      <li className="nav-li">Projetos</li>
-    </ul>
-  </nav>
+  return (
+    <nav className="navbar">
+      <ul className="nav-ul">
+        <Link className="logo" href={"/"}>
+          <p>FIRE</p>
+        </Link>
+        <Link className="nav-li" href={"/"}>
+          Home
+        </Link>
+        <Link className="nav-li" href={"/"}>
+          Sobre
+        </Link>
+        <Link className="nav-li" href={"/"}>
+          Soluções
+        </Link>
+        <Link className="nav-li" href={"/contact"}>
+          Contato
+        </Link>
+      </ul>
+    </nav>
+  );
 }
