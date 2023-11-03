1f022e7609a9a7b01660db10f219c65fde6aee6a
DiogoBlanco <diogoblanco@outlook.com.br>
2023-11-02 21:29:36 -0300

    css

--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -2,6 +2,9 @@ import "./styles.css";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../public/FIREFLY.webp";
+import facebook from "../../public/facebook.svg";
+import instagram from "../../public/instagram.svg";
+import whatsapp from "../../public/whatsapp.svg";
 
 export default function Navbar() {
   return (
@@ -28,6 +31,9 @@ export default function Navbar() {
         <Link className="nav-li" href={"/contact"}>
           CONTATO
         </Link>
+        <Link href={"/"}><Image src={facebook} alt='facebook-logo'></Image></Link>
+        <Link href={"/"}><Image src={instagram} alt='instagram-logo'></Image></Link>
+        <Link href={"/"}><Image src={whatsapp} alt='whatsapp-logo'></Image></Link>
       </ul>
     </nav>
   );
